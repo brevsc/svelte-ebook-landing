955eeb3370f616f430f3875578eadf49aab7bf2c
Breno Vasconcelos <theyukidjr@gmail.com>
2025-04-10 12:24:03 -0300

    feat: add button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  let {children, ...props} = $props();
+</script>
+
+<button {...props}>{@render children()}</button>
+
+<style>
+  button {
+    background-color: #000000;
+    color: #ffffff;
+    padding: 20px 24px;
+    font-weight: normal;
+    font-size: 22px;
+    text-transform: uppercase;
+    transition: all 0.3s;
+    border: 1px solid #ffffff;
+    &:hover {
+      background-color: #ffffff;
+      color: #000000;
+    }
+  }
+</style>
