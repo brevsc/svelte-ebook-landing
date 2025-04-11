4d982143c53a137668c08e4196cceabb0897a246
Breno Vasconcelos <theyukidjr@gmail.com>
2025-04-11 16:22:51 -0300

    feat: add chapter preview component

---
+++ src/lib/components/ChapterPreview.svelte
@@ -0,0 +1,71 @@
+<section class="chapter-preview default-margin">
+	<h2 class="mb-l">What you're getting</h2>
+	<div class="chapter-container">
+		<ul>
+			<li>
+				<button class="chapter-title selected-chapter-title" aria-controls="chapter-info-1" aria-expanded="true">
+					<h3>Chapter 1: Navigating the Spanish Bureaucracy</h3>
+				</button>
+			</li>
+		</ul>
+		<div class="chapter-info">
+			<h3 class="chapter-strapline italic mb-s">
+				Understanding Visas, Residency and Legal Essentrials.
+			</h3>
+			<p>
+				Moving to Spain involves navigating a labyrinth of bureaucratic processes that can feel
+				overwhelming at first. This chapter breaks down the essential steps required to secure your
+				visa, establish residency, and handle other legal formalities. Whether you're planning a
+				short-term stay or seeking permanent residency, understanding Spain's legal framework is
+				crucial. We'll explore the different types of visas available, from student to work visas,
+				and provide tips on how to streamline your application process. Additionally, this chapter
+				covers essential documentation, such as obtaining a Número de Identidad de Extranjero (NIE),
+				which is your key to unlocking many services in Spain. We'll also delve into the process of
+				registering with the local town hall, known as empadronamiento, and the importance of
+				understanding the tax implications of your move. Whether you're a retiree, a digital nomad,
+				or someone relocating for work, this chapter will guide you through the maze of paperwork
+				and official procedures with practical advice and insider tips, ensuring that you're legally
+				prepared for your new life in Spain.
+			</p>
+		</div>
+	</div>
+</section>
+
+<style>
+	.chapter-preview {
+		padding: 80px 0;
+		margin-left: 12vw;
+		margin-right: 20vw;
+		max-width: 1200px;
+	}
+
+	.chapter-container {
+		display: flex;
+		justify-content: space-between;
+		& ul {
+			width: 40%;
+		}
+	}
+
+	.chapter-info {
+		width: 55%;
+		text-align: justify;
+	}
+
+	.chapter-title {
+		border-bottom: 1px solid grey;
+		width: 100%;
+		display: block;
+		padding: 12px;
+		text-align: left;
+	}
+
+	.selected-chapter-title {
+		background-color: #000000;
+		border: none;
+		color: #ffffff;
+		box-shadow:
+			0 4px 6px rgba(0, 0, 0, 0.1),
+			0 1px 3px rgba(0, 0, 0, 0.08);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-  import {HeroSection} from '$components';
+	import { HeroSection, ChapterPreview } from '$components';
 </script>
 
 <HeroSection />
+
+<ChapterPreview />
